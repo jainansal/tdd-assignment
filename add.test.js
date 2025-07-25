5de57cd0a82b83b65a75b1f50955e566d1d87411
Ansal Jain <jainansal@gmail.com>
2025-07-25 21:17:34 +0530

    Add test for single number input in add function

--- add.test.js
+++ add.test.js
@@ -4,3 +4,7 @@ import { add } from "./add";
 test("empty string adds to 0", () => {
   expect(add("")).toBe(0);
 });
+
+test("single number returns number itself", () => {
+  expect(add("1")).toBe(1);
+});
