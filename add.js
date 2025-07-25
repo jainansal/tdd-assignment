3e636ad0d8eaa908700eec4ad72a36d8fb841c3b
Ansal Jain <jainansal@gmail.com>
2025-07-25 21:56:53 +0530

    Update add function to handle adding custom delimiter

--- add.js
+++ add.js
@@ -1,7 +1,14 @@
-export function add(nums) {
-  if (nums.length > 0) {
+export function add(numsString) {
+  if (numsString.length > 0) {
+    var delimiter = "";
+    var nums = numsString;
+    if (nums.startsWith("//")) {
+      delimiter = nums.split("\n")[0].substring(2);
+      nums = nums.split("\n")[1];
+    }
     // /[\n,]+/ is a regex which corresponds to either '\n' or ','
-    const numsSplit = nums.split(/[\n,]+/);
+    const numsSplit =
+      delimiter == "" ? nums.split(/[\n,]+/) : nums.split(delimiter);
     const sum = numsSplit.reduce((res, num) => {
       return res + parseInt(num);
     }, 0);
