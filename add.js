d4c5d3c530d7ca76f8a3a48581c50838f4b96b90
Ansal Jain <jainansal@gmail.com>
2025-07-25 21:25:49 +0530

    Update add function to handle addition of two numbers

--- add.js
+++ add.js
@@ -1,6 +1,10 @@
 export function add(nums) {
   if (nums.length > 0) {
-    return parseInt(nums);
+    const numsSplit = nums.split(",");
+    if (numsSplit.length == 1) {
+      return parseInt(numsSplit[0]);
+    }
+    return parseInt(numsSplit[0]) + parseInt(numsSplit[1]);
   }
   return 0;
 }
