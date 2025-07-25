474762f8d6aedc3da1fce8da8df4e1cdba71510e
Ansal Jain <jainansal@gmail.com>
2025-07-25 21:39:15 +0530

    Update add function to handle addition of numbers separated with new line

--- add.js
+++ add.js
@@ -1,6 +1,7 @@
 export function add(nums) {
   if (nums.length > 0) {
-    const numsSplit = nums.split(",");
+    // /[\n,]+/ is a regex which corresponds to either '\n' or ','
+    const numsSplit = nums.split(/[\n,]+/);
     const sum = numsSplit.reduce((res, num) => {
       return res + parseInt(num);
     }, 0);
