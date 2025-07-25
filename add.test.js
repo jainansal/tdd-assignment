2b62cc3ada8c92e0878a97d73479abedbf90a746
Ansal Jain <jainansal@gmail.com>
2025-07-25 21:21:39 +0530

    Add test for addition of two numbers

--- add.test.js
+++ add.test.js
@@ -8,3 +8,7 @@ test("empty string adds to 0", () => {
 test("single number returns number itself", () => {
   expect(add("1")).toBe(1);
 });
+
+test("passing (3,7) returns 10", () => {
+  expect(add("3,7")).toBe(10);
+});
